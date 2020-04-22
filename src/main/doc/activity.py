914984ae656839dd940c7d400e71c75331fec740
kiracookie <kira0109@qq.com>
2020-04-22 20:36:56 +0800

    修改activity数据脚本 修改后的脚本加工出的数据内容

--- src/main/doc/activity.py
+++ src/main/doc/activity.py
@@ -4,5 +4,5 @@ import re
 with open('activity.txt','r') as f:
     data=f.read()
 with open("test.txt","w") as f:
-    data=data.replace(".txt ",",")
-    f.write(re.sub(r'\s*\|\s*',',',data))
+    data=data.replace(".txt ","|")
+    f.write(re.sub(r'\s*\|\s*','|',data))
